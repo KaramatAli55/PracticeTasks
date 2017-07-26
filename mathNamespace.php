24de815ac06c307b140a337da4596d5f9c164294
KaramatAli55 <30407375+KaramatAli55@users.noreply.github.com>
2017-07-26 12:47:11 +0500

    Add files via upload

---
+++ mathNamespace.php
@@ -0,0 +1,23 @@
+<?php
+
+//declare namespace
+
+namespace math;
+
+function sum($x,$y)
+{
+  return $x+$y;
+}
+function sub($x,$y)
+{
+  return $x-$y;
+}
+function mul($x,$y)
+{
+  return $x*$y;
+}
+function div($x,$y)
+{
+  return $x/$y;
+}
+ ?>
